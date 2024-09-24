e8c7974b9cc83332c93bed530194e7aa73959900
avrilfiona29 <avrilfiona29@gmail.com>
2024-09-24 14:49:19 +0530

    Add files via upload

---
+++ TestHuman.java
@@ -0,0 +1,20 @@
+package pkg1;
+class Human{
+    public void job(){
+        System.out.println("Working Professional");}}
+class Teacher extends Human{
+    public void job(){
+        System.out.println("Teacher");}}
+class Doctor extends Human{
+    public void job(){
+        System.out.println("Doctor");}}
+public class TestHuman {
+    public static void main(String[] args){
+        Human amar=new Human();
+        Teacher babu=new Teacher();
+        Doctor chandra=new Doctor();
+        Human dinesh=new Doctor();
+        amar.job();
+        babu.job();
+        chandra.job();
+        dinesh.job();}}
