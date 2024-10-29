7f3743a6ac96421f4fac17f5315f6d27e454b068
avrilfiona29 <avrilfiona29@gmail.com>
2024-10-29 02:35:47 -0700

    Add files via upload

---
+++ TestNestedclass.java
@@ -0,0 +1,13 @@
+class Outer{
+	void display(){
+		System.out.println("Outer class display method");}
+	class Inner{
+		void display(){
+			System.out.println("Inner class display method");}}}
+public class TestNestedclass{
+	public static void main(String[] args){
+		Outer o1=new Outer();
+		o1.display();
+		Outer.Inner i1=o1.new Inner();
+		i1.display();}}
+		
