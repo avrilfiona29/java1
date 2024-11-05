1a15da2ee6c488ec60123ad9639574090b47284e
avrilfiona29 <avril.23cs021@sode-edu.in>
2024-11-05 02:36:29 -0800

    M7.java

--- M7.java
+++ M7.java
@@ -4,7 +4,7 @@ public class M7 {
 
         public static void main(String[] args) {
                 Tools2 t1=new Tools2();
-                System.out.println(t1.reverse("Madan"));
+                System.out.println(t1.reverse("Avril"));
 
 
         }
